5d5858da4feb2d5f87170980e89179ba1e23f9b1
Tincho <tinchox1991@gmail.com>
2023-04-04 21:03:18 -0300

    falta ejer4_completar

---
+++ 4 ejer_extras/Cuentas_Ventana/src/Cuentas/Cuenta.java
@@ -0,0 +1,33 @@
+
+package Cuentas;
+
+
+public class Cuenta {
+    private float saldo;
+    private String titular;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(float saldo, String titular) {
+        this.saldo = saldo;
+        this.titular = titular;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    
+}
---
+++ 4 ejer_extras/Cuentas_Ventana/src/Cuentas/Principal.java
@@ -0,0 +1,13 @@
+
+package Cuentas;
+
+public class Principal {
+
+   
+    public static void main(String[] args) {
+       Ventana vt = new Ventana();
+       vt.setVisible(true);
+       vt.setLocationRelativeTo(null);
+    }
+    
+}
